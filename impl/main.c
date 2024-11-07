4407134a9d3a94c0fecb31fca9bf390fda1d1da5
samirbuch <samir.buch@temple.edu>
2024-11-07 09:58:02 -0500

    Initial commit + 1

---
+++ impl/main.c
@@ -0,0 +1,12 @@
+//
+// Created by Samir Buch on 11/7/24.
+//
+
+#include <stdio.h>
+#include "disk.h"
+
+int main(void) {
+    printf("Creating virtual disk...\n");
+    make_disk("vdisk.iso");
+
+}
---
+++ lib/disk.h
@@ -0,0 +1,50 @@
+#ifndef LIBTUFS_DISK_H
+#define LIBTUFS_DISK_H
+
+/******************************************************************************/
+#define DISK_BLOCKS  8192      /* number of blocks on the disk                */
+#define BLOCK_SIZE   4096      /* block size on "disk"                        */
+
+/******************************************************************************/
+
+/**
+ * Create an empty, virtual disk file
+ * @param name Name of the virtual disk
+ * @return
+ */
+int make_disk(char *name);
+
+/**
+ * Open a virtual disk file
+ * @param name Name of the virtual disk
+ * @return
+ */
+int open_disk(char *name);
+
+/**
+ * Close the active virtual disk file
+ * @return
+ */
+int close_disk();
+
+/**
+ * Write a block of size BLOCK_SIZE to the virtual disk
+ * @param block Block to write to
+ * @param buf Data to write
+ * @return
+ */
+int block_write(int block, char *buf);
+
+/**
+ * Read a block of size BLOCK_SIZE from the virtual disk
+ * @param block Block to read from
+ * @param buf Buffer to store the data
+ * @return
+ */
+int block_read(int block, char *buf);
+
+/******************************************************************************/
+
+#endif
+
+
