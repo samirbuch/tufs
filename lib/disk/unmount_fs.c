6da7618a26c03679af7563cab22546b57fd1a65d
samirbuch <samir.buch@temple.edu>
2024-11-16 10:44:37 -0500

    lib/unmount_fs: use new sync_fs function

--- lib/disk/unmount_fs.c
+++ lib/disk/unmount_fs.c
@@ -3,13 +3,27 @@
 //
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "disk.h"
 
 int unmount_fs() {
     // TODO: Implement saving in-memory data to disk. For now, this is fine.
 
+    if(!handle || !active) {
+        perror("disk not mounted");
+        return TUFS_ERROR;
+    }
+
+    // Sync changes in memory to disk
+    if(sync_fs() == TUFS_ERROR) {
+        perror("failed to sync disk");
+        return TUFS_ERROR;
+    }
+
     free(p_fat);
     p_fat = NULL;
+    free(p_root);
+    p_root = NULL;
 
     return TUFS_SUCCESS;
 }
