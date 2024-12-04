7893a5293de2f75ce66e38f0778a9b5163ae2660
samirbuch <samir.buch@temple.edu>
2024-12-03 23:26:27 -0500

    lib fs_truncate: free unused blocks

--- lib/tufsio/fs_truncate.c
+++ lib/tufsio/fs_truncate.c
@@ -31,6 +31,34 @@ int fs_truncate(tufs_fd_t file_descriptor, tufs_off_t length) {
         return TUFS_ERROR;
     }
 
+    // Calculate the current number of blocks and the new number of blocks
+    size_t current_blocks = (file->file_size + BLOCK_SIZE - 1) / BLOCK_SIZE; // Ceil division
+    size_t new_blocks = (length + BLOCK_SIZE - 1) / BLOCK_SIZE;           // Ceil division
+
+    // If the new size requires fewer blocks, free the excess blocks
+    if (new_blocks < current_blocks) {
+        uint16_t current_block = file->starting_cluster;
+
+        // Traverse to the last block of the new size
+        for (size_t i = 1; i < new_blocks && current_block != 0xFFFF; i++) {
+            current_block = p_fat->table[current_block];
+        }
+
+        // If there are more blocks to free, unlink them from the chain
+        if (current_block != 0xFFFF) {
+            uint16_t next_block = p_fat->table[current_block];
+            p_fat->table[current_block] = 0xFFFF; // Mark the end of the file
+
+            // Free the remaining blocks
+            while (next_block != 0xFFFF) {
+                uint16_t temp = next_block;
+                next_block = p_fat->table[next_block];
+                p_fat->table[temp] = 0xFFFF; // Unlink the block
+                p_fat->block_status[temp] = EMPTY; // Mark as free
+            }
+        }
+    }
+
     // Set the file's size to the length
     file->file_size = length;
 
